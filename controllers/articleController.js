f66fbe32f8ed9b6200bf0fa46abbe8696641c23b
ferretisDev <ferretis.dev@outlook.com>
2025-08-24 15:25:25 -0600

    Update error image message and imports

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -10,7 +10,7 @@ const article = {
 
       if (err) {
         result.success = false;
-        result.error = "Solo se permiten archivos de imagen (.png, .jpg, .jpeg)";
+        result.error = err.message;
         return res.status(400).json(result);
       }
 
